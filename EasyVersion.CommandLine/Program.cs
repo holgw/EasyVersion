9424fe3acc29ad275e038ed6bf12564fdbe95b38
hozhatelev <hozhatelev@gk-it-consult.ru>
2020-07-02 09:52:23 +0400

    CommandLine: implement hook handle methods

--- EasyVersion.CommandLine/Program.cs
+++ EasyVersion.CommandLine/Program.cs
@@ -1,5 +1,6 @@
 ﻿using CommandLine;
 using EasyVersion.Core;
+using EasyVersion.HookHandlers;
 using System;
 
 namespace EasyVersion.CommandLine
@@ -8,11 +9,17 @@ namespace EasyVersion.CommandLine
     {
         public class Options
         {
+            [Option('c', "command", Required = true, HelpText = "Type of command (<IncrementVersion> or <HookHandle>)")]
+            public string CommandType { get; set; }
+
             [Option('d', "directory", Required = true, HelpText = "Solution directory path")]
             public string Directory { get; set; }
 
-            [Option('i', "increment", Required = true, HelpText = "Incrementation stryle (major, minor or patch)")]
+            [Option('i', "increment", HelpText = "Incrementation style (major, minor or patch)")]
             public string IncrementType { get; set; }
+
+            [Option('h', "hook", HelpText = "Processed git-hook name (<post-merge> or <pre-commit>)")]
+            public string HookName { get; set; }
         }
 
         static void Main(string[] args)
@@ -20,17 +27,33 @@ namespace EasyVersion.CommandLine
             Parser.Default.ParseArguments<Options>(args)
                .WithParsed<Options>(o =>
                {
-                   switch (o.IncrementType)
+                   if (o.CommandType == "IncrementVersion")
                    {
-                       case "major": ExecutePost(o.Directory, IncrementType.IncrementMajor); break;
-                       case "minor": ExecutePost(o.Directory, IncrementType.IncrementMinor); break;
-                       case "patch": ExecutePost(o.Directory, IncrementType.IncrementPatch); break;
-                       default: Console.WriteLine("Unknown Increment Type"); Console.ReadLine(); break;
+                       switch (o.IncrementType)
+                       {
+                           case "major": IncrementVersion(o.Directory, IncrementType.IncrementMajor); break;
+                           case "minor": IncrementVersion(o.Directory, IncrementType.IncrementMinor); break;
+                           case "patch": IncrementVersion(o.Directory, IncrementType.IncrementPatch); break;
+                           default: Console.WriteLine("Unknown Increment Type"); break;
+                       }
+                   }
+                   else if (o.CommandType == "HookHandle")
+                   {
+                       switch (o.HookName)
+                       {
+                           case "post-merge": MasterMerge_PostMergeHookHandler.Run(o.Directory); break;
+                           case "pre-commit": DevelopCommit_PreCommitHookHandler.Run(o.Directory); break;
+                           default: Console.WriteLine("Unknown Hook Name"); break;
+                       }
+                   }
+                   else
+                   {
+                       Console.WriteLine("Unknown Command Type"); 
                    }
                });            
         }
 
-        static void ExecutePost(string dir, IncrementType incrementType)
+        static void IncrementVersion(string dir, IncrementType incrementType)
         {
             var mng = Factory.AssemblyInfo.GetManager();
             mng.IncrementVersions(dir, incrementType);
