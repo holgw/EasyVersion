1ec60d0fc1ceef487169a8e02e8c4990dd58c61e
hozhatelev <hozhatelev@gk-it-consult.ru>
2020-07-02 09:50:56 +0400

    added HookHandlers project

---
+++ EasyVersion.HookHandlers/Handlers/PostMergeHookHandler.cs
@@ -0,0 +1,40 @@
+﻿using EasyVersion.Core;
+using LibGit2Sharp;
+using System;
+using System.Linq;
+
+namespace EasyVersion.HookHandlers
+{
+    /// <summary>
+    /// Инкрементирует значение MINOR в версии перед слиянием в master
+    /// </summary>
+    public class MasterMerge_PostMergeHookHandler
+    {
+        public static void Run(string repoDir)
+        {
+            var repo = new Repository(repoDir);
+            string branchName = repo.Head.FriendlyName;
+            Console.WriteLine($"Current branch: {branchName}");
+
+            if (repo.Head.FriendlyName != "master")
+            {
+                Console.WriteLine($"Current branch is not <master>. Processing stopped.");
+                return;
+            }
+
+            var versionMng = Factory.AssemblyInfo.GetManager();
+            string[] changedFiles = versionMng.IncrementVersions(repoDir, IncrementType.IncrementMinor);
+
+            // Зафиксируем новые изменения
+            if (changedFiles.Any())
+                Commands.Stage(repo, changedFiles);
+
+            // Закоммитим изменения
+            Signature author = new Signature("git-hook", "@none", DateTime.Now);
+            Signature committer = author;
+            Commit commit = repo.Commit("[AUTO COMMIT] Increment MAJOR version", author, committer);
+
+            Console.WriteLine($"Processing finished succesfully");
+        }
+    }
+}
---
+++ EasyVersion.HookHandlers/Handlers/PreCommitHookHandler.cs
@@ -0,0 +1,35 @@
+﻿using EasyVersion.Core;
+using LibGit2Sharp;
+using System;
+using System.Linq;
+
+namespace EasyVersion.HookHandlers
+{
+    /// <summary>
+    /// Инкрементирует значение PATCH в версии перед коммитом в develop
+    /// </summary>
+    public class DevelopCommit_PreCommitHookHandler
+    {
+        public static void Run(string repoDir)
+        {
+            var repo = new Repository(repoDir);
+            string branchName = repo.Head.FriendlyName;
+            Console.WriteLine($"Current branch: {branchName}");
+
+            if (repo.Head.FriendlyName != "develop")
+            {
+                Console.WriteLine($"Current branch is not <develop>. Processing stopped.");
+                return;
+            }
+
+            var versionMng = Factory.AssemblyInfo.GetManager();
+            string[] changedFiles = versionMng.IncrementVersions(repoDir, IncrementType.IncrementPatch);
+
+            // Зафиксируем новые изменения
+            if (changedFiles.Any())
+                Commands.Stage(repo, changedFiles);
+
+            Console.WriteLine($"Processing finished succesfully");
+        }
+    }
+}
